a7b0eedd2248873528ebebfc5595345a8d256550
Sushil Gupta <kalwarsushil2@gmail.com>
2023-03-21 07:56:45 +0545

    third

--- utils/jwt.js
+++ utils/jwt.js
@@ -12,10 +12,11 @@ const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET)
 const attachCookiesToResponse = ({ res, user }) => {
   const token = createJWT({ payload: user })
   const oneDay = 1000 * 60 * 60 * 24
+  const exDate = new Date(Date.now + oneDay)
 
   res.cookie('token', token, {
     httpOnly: true,
-    expires: new Date(Date.now + oneDay),
+    expires: exDate,
     signed: true,
     domain: 'http://localhost:3000/',
     sameSite: 'none',
